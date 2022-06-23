700365f1a5d0572faa382544d6dc8dc84b59ed0e
Bruk Lemma <bruklemma2017@gmail.com>
2022-06-23 16:35:48 +0300

    created basic controller using the @RequestMapping

--- src/main/java/com/example/demo/Bruk.java
+++ src/main/java/com/example/demo/Bruk.java
@@ -1,8 +1,13 @@
 package com.example.demo;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Bruk  {
 
+    @RequestMapping("/bruk")
+    public String sayhi(){
+        return  "hi bruk";
+    }
 }
