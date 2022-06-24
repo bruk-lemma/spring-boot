a591d498a0974905b2c53cd45bab8edb7896d600
Bruk Lemma <bruklemma2017@gmail.com>
2022-06-24 14:59:49 +0300

    implemented simple local getTopic,updateTopic,and deleteopic

--- src/main/java/com/example/demo/TopicService.java
+++ src/main/java/com/example/demo/TopicService.java
@@ -1,18 +1,45 @@
 package com.example.demo;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class TopicService {
-    private List<Topic> topics= Arrays.asList(
-        new Topic("spring","Spring Boot","Spring description"),
-                new Topic("spring","Spring Boot","Spring description"),
-                new Topic("spring","Spring Boot","Spring description")
-        );
+    private List<Topic> topics= new ArrayList<>(Arrays.asList(
+                new Topic("spring","Spring framework","Spring description"),
+                new Topic("java","Core Java","Java description"),
+                new Topic("boot","Spring Boot","Boot description")
+        ));
        public List<Topic> getAllTopics(){
            return topics;
        }
+
+       public Topic getTopic(String id){
+           return topics.stream().filter(t ->t.getId().equals(id)).findFirst().get();
+       }
+
+    public void addTopic(Topic topic) {
+           topics.add(topic);
+    }
+
+    public void updateTopic(Topic topic, String id){
+           for(int i=0;i<topics.size();i++){
+               Topic t=topics.get(i);
+               if(t.getId().equals(id)){
+                   topics.set(i,topic);
+                   return;
+               }
+           }
+    }
+
+    public void deleteTopic(String id){
+       topics.removeIf(t -> t.getId().equals(id));
+        }
+
+
 }
